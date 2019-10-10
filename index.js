2d7986aca9f621db5b52ece8c6eb1385f88b2585
Abagail Hudson <abagailanne@gmail.com>
2019-10-10 00:19:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,26 +21,4 @@ function add2(n) {
 }
 //git@github.com/learn-co-curriculum/javascript-fix-the-scope-lab.git"
 
-  "keywords" [
-    "javascript",
-    "flatiron",
-    "learn"
-  ],
-  "author": "pletcher",
-  "license": "SEE LICENSE IN LICENSE.md",
-  "bugs": {
-    "url": "https://github.com/learn-co-curriculum/javascript-fix-the-scope-lab/issues"
-  },
-  "homepage": "https://github.com/learn-co-curriculum/javascript-fix-the-scope-lab#readme",
-  "devDependencies": {
-    "babel-core": "^6.11.4",
-    "babel-preset-es2015": "^6.9.0",
-    "chai": "^3.5.0",
-    "expect": "^1.20.2",
-    "jsdom": "^8.5.0",
-    "mocha": "^2.4.5",
-    "mocha-jsdom": "~1.1.0",
-    "mocha-multi": "^0.9.0"
-  },
-  "dependencies": {}
-}
+  
